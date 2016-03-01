a2ef6dec4a04ffe08bed4a9da1f0bbb594c636ed
Nick Bristow <Nick.Bristow@gsa.gov>
2016-03-01 14:40:03 -0500

    made snapshot of database so i can transfer to postgress

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -6,4 +6,14 @@ class IssuesController < ApplicationController
    
     redirect_to site_path(@site)
   end
+
+  def create
+    @site = Site.find(params[:site_id])
+    @issue = @site.issues.create(issue_params)
+    redirect_to site_path(@site)
+  end
+  private
+    def issue_params
+        params.require(:issue).permit(:github_id)
+    end
 end
